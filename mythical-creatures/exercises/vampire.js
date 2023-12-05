1ad3c12f6ff0609f72979f102724537b1278d105
David Kwon <dkwon1223@gmail.com>
2023-12-04 23:44:44 -0700

    Implement createVampire function in vampire.js and pass corresponding tests

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -1,7 +1,30 @@
+//----------------------------------------------//
+// createVampire
+/*
+Requirements:
+- Inputs: name string, pet string(default value = "bat"), boolean for thirsty(default value: true), number of ounces of blood drank(default value: 0)
+- Outputs: object of vampire with properties aligning with arguments passed into parameters
 
+Pseudocode:
+- DEFINE FUNCTION: createVampire() with PARAMETERS: stringName, stringPet = "bat", booleanThirsty = true, numOuncesDrank
+- INITIALIZE OBJECT VARIABLE: vampireObject 
+- KEYS: name, pet, thirsty, ouncesDrank
+- VALUES: stringName, stringPet, booleanThirsty, numOuncesDrank
+- RETURN: vampireObject
+*/
 
+function createVampire(stringName, stringPet = "bat", booleanThirsty = true, numOuncesDrank) {
+  let vampireObject = {
+    name: stringName,
+    pet: stringPet,
+    thirsty: booleanThirsty,
+    ouncesDrank: numOuncesDrank
+  }
+  return vampireObject;
+}
+//----------------------------------------------//
 module.exports = {
-  // createVampire, 
+  createVampire, 
   // drink, 
   // findBatLovers, 
   // encounterDeliciousVictim, 
