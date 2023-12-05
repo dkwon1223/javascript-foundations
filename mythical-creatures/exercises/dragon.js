d1aaad7fdc26569679aa58bca4aafb0e34a6d716
David Kwon <dkwon1223@gmail.com>
2023-12-04 22:26:21 -0700

    Implement createDragon function in dragon.js and pass corresponding tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,7 +1,30 @@
+//----------------------------------------------//
+// createDragon
+/*
+Requirements:
+- Input(s): 3 string arguments passed into corresponding parameters
+- Outputs(s): object with properties corresponding to arguments passed into parameters
+
+Pseudocode:
+- DEFINE FUNCTION: createDragon() with PARAMETERS: nameString, riderString, tempermentString
+- INITIALIZE OBJECT: dragonObject 
+- KEYS: name, rider, temperment
+- VALUES: nameString, riderString, tempermentString
+- RETURN: dragonObject
+*/
+
+function createDragon(nameString, riderString, tempermentString) {
+  let dragonObject = {
+    name: nameString,
+    rider: riderString,
+    temperment: tempermentString
+  };
+  return dragonObject;
+}
 
 
 module.exports = {
-  // createDragon, 
+  createDragon, 
   // greetRider, 
   // eat, 
   // findFireBreathers
