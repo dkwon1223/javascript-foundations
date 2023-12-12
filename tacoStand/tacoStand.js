0688226ba8d2a41c63d7134190be7074d2c8d120
David Kwon <dkwon1223@gmail.com>
2023-12-11 22:47:40 -0700

    Implement tacoStand.js solutions and pass corresponding tests

--- tacoStand/tacoStand.js
+++ tacoStand/tacoStand.js
@@ -1,2 +1,38 @@
+function createIngredient(stringName = "unknown", numPrice = 0.00) {
+    let ingredient = {
+        name: stringName,
+        price: numPrice
+    };
+    return ingredient;
+}
 
-module.exports = {}
+function createTaco(stringName = "custom", arrayIngredients = []) {
+    let taco = {
+        name: stringName,
+        ingredients: arrayIngredients
+    };
+    return taco;
+}
+
+function addIngredientToTaco(objectTaco, objectIngredient = null) {
+    if(objectIngredient !== null) {
+        objectTaco.ingredients.push(objectIngredient);
+        return objectTaco;
+    } else {
+        return objectTaco;
+    }
+}
+
+function calculatePrice(objectTaco) {
+    let totalPrice = 0;
+    for (let i = 0; i < objectTaco.ingredients.length; i++) {
+        totalPrice += objectTaco.ingredients[i].price;
+    }
+    return totalPrice;
+}
+module.exports = {
+    createIngredient,
+    createTaco,
+    addIngredientToTaco,
+    calculatePrice
+}
