4a4bdcee9c6dda286b2738b93404eb01be5d9639
David Kwon <dkwon1223@gmail.com>
2023-12-11 23:20:29 -0700

    Implement elevator.js solutions and pass corresponding tests

--- elevator/elevator.js
+++ elevator/elevator.js
@@ -1,3 +1,34 @@
+function createElevator(stringBuilding, numFloors, numCurrentFloor, arrayPassengers) {
+    let elevator = {
+        building: stringBuilding,
+        floors: numFloors,
+        currentFloor: numCurrentFloor,
+        passengers: arrayPassengers
+    };
+    return elevator;
+}
+
+function changeFloors(objectElevator, numFloor) {
+    if(objectElevator.currentFloor === numFloor) {
+        return `You're already on floor ${numFloor}!`;
+    } else if(numFloor > objectElevator.floors || numFloor < 1) {
+        return `Floor ${numFloor} does not exist!`;
+    } else {
+        objectElevator.currentFloor = numFloor;
+        return `Taking you to floor ${numFloor}!`;
+    }
+}
+
+function dropOffPassenger(objectElevator, stringPassenger) {
+    passengers = objectElevator.passengers;
+    targetPassenger = stringPassenger;
+    passengers.splice(passengers.indexOf(targetPassenger), 1);
+    return passengers;
+}
 
 
-module.exports = {  };
+module.exports = { 
+    createElevator,
+    changeFloors,
+    dropOffPassenger
+};
