da649ffd9193f403ef3cf62e4f1bcf8a4dcc3ac3
David Kwon <dkwon1223@gmail.com>
2023-12-11 23:03:00 -0700

    Implement mealPlanning.js solutions and pass corresponding tests

--- meal-planning/mealPlanning.js
+++ meal-planning/mealPlanning.js
@@ -1,2 +1,32 @@
+function createMeal(stringType, numCalGoal, arrayDishes = []) {
+    let meal = {
+        type: stringType,
+        calorieGoal: numCalGoal,
+        dishes: arrayDishes
+    };
+    return meal;
+}
 
-module.exports = { }
+function addDish(objectMeal, objectDish) {
+    if(objectDish.calories > objectMeal.calorieGoal) {
+        return objectMeal;
+    } else {
+        objectMeal.dishes.push(objectDish);
+        objectMeal.calorieGoal -= objectDish.calories;
+        return objectMeal;
+    }
+}
+
+function calculateCalories(objectMeal) {
+    totalCalories = 0;
+    for(let i = 0; i < objectMeal.dishes.length; i++) {
+        totalCalories += objectMeal.dishes[i].calories;
+    }
+    return `${objectMeal.type} has a total of ${totalCalories} calories.`;
+}
+
+module.exports = { 
+    createMeal,
+    addDish,
+    calculateCalories
+};
