1fe83575c0f36cda98e43883cb89f6f9e1aaf943
David Kwon <dkwon1223@gmail.com>
2023-12-11 22:26:04 -0700

    Implement birthday.js solutions and pass corresponding tests

--- birthdays/birthdays.js
+++ birthdays/birthdays.js
@@ -1,3 +1,28 @@
+function createBirthday(stringName, numMonth, numDay) {
+    let birthday = {
+        name: stringName,
+        month: numMonth,
+        day: numDay
+    };
+    return birthday;
+}
 
+function celebrateBirthday(objectBirthday) {
+    return `Today is ${objectBirthday.month}/${objectBirthday.day}! Happy birthday, ${objectBirthday.name}!`;
+}
 
-module.exports = {  };
+function countBirthdays(arrayBirthdays, numMonth) {
+    monthCounter = 0;
+    for(let i = 0; i < arrayBirthdays.length; i++) {
+        if(arrayBirthdays[i].month === numMonth) {
+            monthCounter++;
+        }
+    }
+    return monthCounter;
+}
+
+module.exports = {  
+    createBirthday,
+    celebrateBirthday,
+    countBirthdays
+};
