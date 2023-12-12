b3abadd4e348ab2f2ba01ae82c3e42f0bdb3da69
David Kwon <dkwon1223@gmail.com>
2023-12-11 21:16:25 -0700

    Implement video-games.js solutions

--- video-games/video-games.js
+++ video-games/video-games.js
@@ -1,4 +1,45 @@
+function createPlayer(stringName, numAge, arrayMoves) {
+    let player = {
+        name: stringName,
+        age: numAge,
+        moveset: arrayMoves
+    };
+    return player; 
+}
 
+function createLevel(stringName, arrayPlayers) {
+    let level = {
+        name: stringName,
+        players: arrayPlayers,
+        coins: 0,
+        lives: 3
+    };
+    return level;
+}
 
+function findCoins(objectLevel, numCoins) {
+    if((numCoins + objectLevel.coins) >= 100) {
+        objectLevel.coins += numCoins;
+        objectLevel.lives ++;
+        return objectLevel;
+    } else {
+        objectLevel.coins += numCoins;
+        return objectLevel;
+    } 
+}
 
-module.exports = { };
+function defeatPlayer(objectLevel) {
+    if(objectLevel.lives > 1) {
+        objectLevel.lives --;
+        return objectLevel;
+    } else {
+        return `GAME OVER`;
+    }
+}
+
+module.exports = { 
+    createPlayer,
+    createLevel,
+    findCoins,
+    defeatPlayer
+};
