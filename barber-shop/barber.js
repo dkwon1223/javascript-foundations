c84ca5d475632f0b91b293fe4cd9efb8d66c9edd
David Kwon <dkwon1223@gmail.com>
2023-12-18 15:55:18 -0700

    Implement barber.js solutions and pass all corresponding tests

--- barber-shop/barber.js
+++ barber-shop/barber.js
@@ -1,3 +1,37 @@
+function createBarber(stringName, numEarnings = 0, arrayCuts = []) {
+    let barber = {
+        name: stringName,
+        earnings: numEarnings,
+        haircuts: arrayCuts
+    }
+    return barber;
+}
+
+function giveCompliment(styleObject) {
+    return `This ${styleObject.style} looks great!`;
+}
+
+function cutHair(barberObject, styleObject) {
+    barberObject.haircuts.push(styleObject);
+    barberObject.earnings += styleObject.price;
+    return barberObject;
+}
+
+function listStyles(barberObject, stringStyle) {    
+    let matchingStyles = [];
+    for(let i = 0; i < barberObject.haircuts.length; i++) {
+        if(barberObject.haircuts[i].hairLength === stringStyle) {
+            matchingStyles.push(barberObject.haircuts[i].style);
+        }
+    }
+    return matchingStyles;
+}
 
 
-module.exports = {  };
+
+module.exports = { 
+    createBarber,
+    giveCompliment,
+    cutHair,
+    listStyles
+};
