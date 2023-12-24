f30285c5acbc473507e673050a10e546ce72d31d
David Kwon <dkwon1223@gmail.com>
2023-12-23 17:37:28 -0700

    Implement vhs.js solutions and pass corresponding tests

--- vhs/vhs.js
+++ vhs/vhs.js
@@ -1,3 +1,36 @@
+function createTape(stringTitle, booleanReadyToPlay = false) {
+    let tape = {
+        title: stringTitle,
+        readyToPlay: booleanReadyToPlay
+    };
+    return tape;
+}
 
+function reset(objectTape) {
+    objectTape.readyToPlay = true;
+    return objectTape;
+}
 
-module.exports = {  }
+function createCollection(...tapeObject) {
+    if(tapeObject.length === 0) {
+        return "Your collection is empty.";
+    } else {
+        return tapeObject;
+    }
+}
+
+function previewCollection(collection) {
+    let preview = [];
+    for(let i = 0; i < collection.length; i++) {
+        preview.push(collection[i].title);
+    }
+    return preview;
+}
+
+module.exports = { 
+    createTape,
+    reset,
+    createCollection,
+    previewCollection
+}
+
