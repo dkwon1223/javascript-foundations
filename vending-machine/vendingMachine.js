a748d7e2378732bec5fc04b0963ce7e6a0bd22d6
David Kwon <dkwon1223@gmail.com>
2023-12-23 17:13:20 -0700

    Implement vendingMachine.js solutions and pass all corresponding tests

--- vending-machine/vendingMachine.js
+++ vending-machine/vendingMachine.js
@@ -1,2 +1,33 @@
+function createItemStock(stringName = "unknown", numQuantity = 0, numPrice = 1.00)  {
+    let item = {
+        name: stringName,
+        quantity: numQuantity,
+        price: numPrice
+    };
+    return item;
+}
 
-module.exports = {}
+function makePurchase(objectItem, numPayment) {
+    if(numPayment < objectItem.price) {
+        return `Sorry, you need at least $${objectItem.price} to make that purchase`;
+    } else if (objectItem.quantity === 0) {
+        return `Sorry, there are none left`;
+    } else {
+        return `Here are your ${objectItem.name}`;
+    }
+}
+
+function collectChange(arrayCoins) {
+    let total = 0;
+    for(let i = 0; i < arrayCoins.length; i++) {
+        total += arrayCoins[i];
+    }
+    return total;
+}
+
+
+module.exports = {
+    createItemStock,
+    collectChange,
+    makePurchase
+}
