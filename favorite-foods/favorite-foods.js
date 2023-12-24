48e2119c8fd00a72a8304565ffcf09c10b604359
David Kwon <dkwon1223@gmail.com>
2023-12-23 18:08:54 -0700

    Implement favorite-foods.js solutions and pass corresponding tests

--- favorite-foods/favorite-foods.js
+++ favorite-foods/favorite-foods.js
@@ -1,4 +1,37 @@
+function createFavoriteFood(objectDish) {
+    let favFood = {
+        name: objectDish.dish,
+        ingredients: objectDish.ingredients,
+        isSpicy: objectDish.isSpicy,
+        timesOrdered: 0
+    }
+    return favFood;
+}
 
+function commentOnSpiciness(objectFood) {
+    if(objectFood.isSpicy === true) {
+        return `Wow, this ${objectFood.name} is so spicy!`;
+    } else {
+        return `Phew, this ${objectFood.name} is not very spicy.`;
+    }
+}
 
+function orderFood(objectFood) {
+    objectFood.timesOrdered++;
+    return objectFood;
+}
 
-module.exports = { };
+function createShoppingList(arrayFoodObjects) {
+    let list = [];
+    for(let i = 0; i < arrayFoodObjects.length; i++) {
+        list = list.concat(arrayFoodObjects[i].ingredients);
+    }
+    return list;
+}
+
+module.exports = { 
+    createFavoriteFood,
+    commentOnSpiciness,
+    orderFood,
+    createShoppingList
+}
