e5bd88d34d525a671710b6a78904221459ed3cb0
David Kwon <dkwon1223@gmail.com>
2024-01-09 20:11:06 -0700

    Implement solutions for dj.js and pass all corresponding tests

--- dj/dj.js
+++ dj/dj.js
@@ -1,8 +1,64 @@
+/*
+Requirements: 
+- Inputs: string, string, and boolean
+- Outputs: object
+
+Pseudocode:
+- DEFINE FUNCTION: createSong() with PARAMETERS: stringTitle, stringArtist, booleanFavorite, booleanPlayed = false
+- INITIALIZE OBJECT VARIABLE: song with KEY VALUE PAIRS:
+  - name: stringTitle
+  - artist: stringArtist
+  - favorite: booleanFavorite
+  - hasBeenPlayed: booleanPlayed
+- RETURN song
+*/
+
+function createSong(stringTitle, stringArtist, booleanFavorite = false, booleanPlayed = false) {
+  return {
+    name: stringTitle,
+    artist: stringArtist,
+    favorite: booleanFavorite,
+    hasBeenPlayed: booleanPlayed
+  };
+}
+
+function playSong(songObject) {
+  songObject.hasBeenPlayed = true;
+  return songObject;
+}
+
+function makePlaylist(stringName, arraySongs) {
+  return {
+    name: stringName,
+    songs: arraySongs
+  };
+}
+
+function addSongToPlaylist(objectPlaylist, objectSong) {
+  objectPlaylist.songs.push(objectSong);
+  return objectPlaylist;
+}
+
+function playSongs(objectPlaylist, stringFavorites) {
+  if(stringFavorites === "favorites only") {
+    for(let i = 0; i < objectPlaylist.songs.length; i++) {
+      if(objectPlaylist.songs[i].favorite) {
+        playSong(objectPlaylist.songs[i]);
+      }
+    }
+    return objectPlaylist;
+  } else {
+    for(let i = 0; i < objectPlaylist.songs.length; i++) {
+      playSong(objectPlaylist.songs[i]);
+    }
+    return objectPlaylist;
+  }
+}
 
 module.exports = { 
-  // createSong, 
-  // playSong, 
-  // makePlaylist, 
-  // addSongToPlaylist, 
-  // playSongs
+  createSong, 
+  playSong, 
+  makePlaylist, 
+  addSongToPlaylist, 
+  playSongs
 };
