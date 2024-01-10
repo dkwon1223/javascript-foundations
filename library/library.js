640df8ec3534dd53cb1b4631b5cd6d60a07f3052
David Kwon <dkwon1223@gmail.com>
2024-01-09 19:31:16 -0700

    Implement solutions in library.js and pass all corresponding tests

--- library/library.js
+++ library/library.js
@@ -1,3 +1,33 @@
+function issueCard(stringName, numAge, numBooksCheckedOut = 0) {
+    let card = {
+        name: stringName,
+        age: numAge,
+        numBooksCheckedOut: numBooksCheckedOut,
+        isChild: null
+    }
+    if(numAge >= 12) {
+        card.isChild = false;
+    } else {
+        card.isChild = true;
+    }
+    return card;
+}
+
+function searchByAuthor(arrayCollection, stringAuthor)  {
+    arrayResults = [];
+    for(let i = 0; i < arrayCollection.length; i++) {
+        if(arrayCollection[i].author === stringAuthor) {
+            arrayResults.push(arrayCollection[i]);
+        }
+    }
+    if(arrayResults.length === 0) {
+        return "No book found for search criteria";
+    }
+    return arrayResults
+}
 
 
-module.exports = { };
+module.exports = { 
+    issueCard,
+    searchByAuthor
+};
